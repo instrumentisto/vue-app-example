a86938b6a4f149c02e2aab980592402d9a79a61a
Roman Dragan <roman@dragan.com.ua>
2017-04-06 09:22:20 +0300

    Implement footer with total registrations info

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,14 +6,21 @@
         <main>
             <router-view></router-view>
         </main>
-        <footer></footer>
+        <footer>
+            <span class="pull-right">Total registrations: <b>{{ usersTotalCount }}</b></span>
+        </footer>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import Component from 'vue-class-component'
-    import Navbar from './partial/Navbar.vue'
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+    import { Getter, Action, namespace } from 'vuex-class';
+
+    import Navbar from './partial/Navbar.vue';
+
+    const UsersGetter = namespace('users', Getter);
+    const UsersAction = namespace('users', Action);
 
     @Component({
         components: {
@@ -21,20 +28,41 @@
         }
     })
     export default class App extends Vue {
-        // Common logic for all pages
+
+        @UsersGetter('totalCount') usersTotalCount;
+
+        @UsersAction('getAll') getAllUsers;
+
+        created(): void {
+            this.getAllUsers();
+        }
     }
 </script>
 
 <style src="../../bower_components/bootstrap/dist/css/bootstrap.css"></style>
 <style>
-    html, body {
+    html, body, #app, main {
         height: 100%;
         width: 100%;
+    }
+
+    main {
+        margin-top: -72px;
+        padding-top: 72px;
     }
 
     main > section {
         width: 400px;
         margin: 0 auto;
         text-align: center;
+    }
+
+    footer {
+        height: 52px;
+        margin-top: -52px;
+        padding: 10px;
+        border-top: 1px solid #e7e7e7;
+        background-color: #f8f8f8;
+        line-height: 32px;
     }
 </style>
