556ba7f450b29c5e8f165cbf0ba05f1022dffd01
Roman Dragan <roman@dragan.com.ua>
2017-04-13 16:19:11 +0300

    Remove sinon dependency

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,7 +3,7 @@ const webpackConfig = require('../../webpack/test.config.js');
 module.exports = function (config) {
     config.set({
         browsers: ['PhantomJS'],
-        frameworks: [/*'jasmine',*/'mocha', 'sinon-chai', /*'phantomjs-shim'*/],
+        frameworks: [/*'jasmine',*/'mocha', 'chai', /*'phantomjs-shim'*/],
         reporters: ['mocha', 'spec'],
         files: ['./index.ts'],
         preprocessors: {
