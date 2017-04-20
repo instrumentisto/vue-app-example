3e5848c23b1572f5f07e8ffda81d439812cabc14
Roman Dragan <roman@dragan.com.ua>
2017-04-20 10:23:27 +0300

    Update Karma config and package.json

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,19 +3,18 @@ const webpackConfig = require('../../webpack/test.config.js');
 module.exports = function (config) {
     config.set({
         browsers: ['PhantomJS'],
-        frameworks: ['mocha', 'chai'/*, 'phantomjs-shim'*/],
+        frameworks: ['mocha', 'chai'],
         reporters: ['spec'],
         files: [
             '../../node_modules/babel-polyfill/dist/polyfill.js',
             './index.ts'
         ],
         preprocessors: {
-            './index.ts': ['webpack'/*, 'sourcemap'*/]
+            './index.ts': ['webpack']
         },
         webpack: webpackConfig,
         webpackMiddleware: {
             noInfo: true,
         },
-        // logLevel: config.LOG_DEBUG
     });
 };
