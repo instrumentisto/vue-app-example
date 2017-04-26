caa4fa9d1e7efef7a95767e234799d33f5bfe56b
Roman Dragan <roman@dragan.com.ua>
2017-04-26 12:04:41 +0300

    Total refactor and add typings

--- src/store/modules/user/mutations.ts
+++ src/store/modules/user/mutations.ts
@@ -2,21 +2,21 @@ import { MutationTree } from 'vuex';
 
 import UserState from 'store/modules/user/state';
 
-export const ADD_USER = 'ADD_USER';
-export const SET_AUTHORIZED_USER = 'SET_AUTHORIZED_USER';
-export const SET_USERS_LIST = 'SET_USERS_LIST';
+export const ADD = 'ADD';
+export const SET_AUTHORIZED = 'SET_AUTHORIZED';
+export const SET_LIST = 'SET_LIST';
 
 export default {
-    [ADD_USER]: (state: UserState, user) => {
+    [ADD]: (state: UserState, user) => {
         state.all.push(user);
     },
-    [SET_AUTHORIZED_USER]: (state: UserState, user) => {
+    [SET_AUTHORIZED]: (state: UserState, user) => {
         if (user && user.password) {
             delete user.password;
         }
         state.authorized = user;
     },
-    [SET_USERS_LIST]: (state: UserState, users) => {
+    [SET_LIST]: (state: UserState, users) => {
         state.all = users;
     },
 } as MutationTree<UserState>;
