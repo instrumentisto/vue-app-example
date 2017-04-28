2c5f8bdbf0d1dc36ac573e14f42a16e2d8f63ca8
Roman Dragan <roman@dragan.com.ua>
2017-04-28 15:29:32 +0300

    Move loading spinned to the separate Vue component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <img v-show="isLoading" src="~assets/img/loading_box.gif">
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+    import { Getter } from 'vuex-class';
+
+    import { LOADING } from 'store/root/getters';
+
+    @Component
+    export default class LoadingSpinner extends Vue {
+
+        @Getter(LOADING)
+        private isLoading;
+
+    }
+</script>
