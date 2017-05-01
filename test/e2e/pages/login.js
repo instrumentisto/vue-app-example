09dd8c99203c46cecc465537538ecc66ce36b3ca
Roman Dragan <roman@dragan.com.ua>
2017-05-01 15:30:25 +0300

    Implement e2e specs for sign up page and improve validation logic

--- test/e2e/pages/login.js
+++ test/e2e/pages/login.js
@@ -1,5 +1,10 @@
 module.exports = {
-  elements: {},
+  elements: {
+    form: '#signInForm',
+    emailInput: 'input[name=email]',
+    passwordInput: 'input[name=password]',
+    error: 'p.error'
+  },
   url: function() {
     return this.api.launchUrl + 'login';
   },
--- test/e2e/specs/login.ts
+++ test/e2e/specs/login.ts
@@ -5,29 +5,30 @@ const tests = {
     Helper.beforeEach(browser, done);
   },
   'Authorization with valid credentials': (client) => {
-    const login = client.page.login();
+    const loginPage = client.page.login();
+    const profilePage = client.page.profile();
 
-    login.navigate()
+    loginPage.navigate()
       .waitForElementVisible('#app', 3000)
-      .setValue('input[name=email]', 'test@gmail.com')
-      .setValue('input[name=password]', '123123')
-      .submitForm('#signInForm')
-      .waitForElementVisible('section#profile', 3000)
-      .assert.containsText('p#user_name', 'Test User')
-      .assert.containsText('p#user_email', 'test@gmail.com');
+      .setValue('@emailInput', 'test@gmail.com')
+      .setValue('@passwordInput', '123123')
+      .submitForm('@form')
+      .waitForElementVisible(profilePage.section.main.selector, 3000)
+      .assert.containsText(profilePage.section.main.elements.userName.selector, 'Test User')
+      .assert.containsText(profilePage.section.main.elements.userEmail.selector, 'test@gmail.com');
 
     client.end();
   },
   'Authorization with wrong credentials': (client) => {
-    const login = client.page.login();
+    const loginPage = client.page.login();
 
-    login.navigate()
+    loginPage.navigate()
       .waitForElementVisible('#app', 3000)
-      .setValue('input[name=email]', 'wronguser@gmail.com')
-      .setValue('input[name=password]', '123123')
-      .submitForm('#signInForm')
-      .waitForElementVisible('p.error', 3000)
-      .assert.containsText('p.error', client.globals.localeData.errors.access_denied);
+      .setValue('@emailInput', 'wronguser@gmail.com')
+      .setValue('@passwordInput', '123123')
+      .submitForm('@form')
+      .waitForElementVisible('@error', 3000)
+      .assert.containsText('@error', client.globals.localeData.errors.access_denied);
 
     client.end();
   },
